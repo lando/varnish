91ab88c9e4c2f36d6181b2ad081514516b26afdc
Alec Reynolds <alec+git@lando.dev>
2023-11-17 11:13:17 -0800

    Feed the linter.

--- builders/varnish-nginx.js
+++ builders/varnish-nginx.js
@@ -1,15 +1,13 @@
 'use strict';
 
 const _ = require('lodash');
-const fs = require('fs');
-const path = require('path');
-const LandoNginx = require('./../../node_modules/@lando/nginx/builders/nginx.js');
+const landoNginx = require('./../../node_modules/@lando/nginx/builders/nginx.js');
 
 // Builder
 module.exports = {
   name: 'varnish-nginx',
   parent: 'nginx',
-  builder: (parent, config) => class VarnishNginx extends LandoNginx(parent, config) {
+  builder: (parent, config) => class VarnishNginx extends landoNginx(parent, config) {
     constructor(id, options = {}) {
       super(id, options, {services: _.set({}, options.name)});
     };
