a2c54f939e1d75f05dd669c951500b620359e94f
Alec Reynolds <alec+git@lando.dev>
2023-11-24 13:40:03 -0800

    Remove hardcoding into registry, correct extends logic.

--- builders/varnish-nginx.js
+++ builders/varnish-nginx.js
@@ -1,13 +1,13 @@
 'use strict';
 
 const _ = require('lodash');
-const landoNginx = require('./../../node_modules/@lando/nginx/builders/nginx.js');
+const landoNginx = require('./../node_modules/@lando/nginx/builders/nginx.js');
 
 // Builder
 module.exports = {
   name: 'varnish-nginx',
-  parent: 'nginx',
-  builder: (parent, config) => class VarnishNginx extends landoNginx(parent, config) {
+  parent: '_service',
+  builder: (parent, config) => class VarnishNginx extends landoNginx.builder(parent, landoNginx.config) {
     constructor(id, options = {}) {
       super(id, options, {services: _.set({}, options.name)});
     };
